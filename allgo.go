d1680c6e029e450b55417b8f60d22a59867fda33
Jaana Burcu Dogan <jbd@google.com>
2016-12-05 12:56:59 -0800

    initial commit

---
+++ allgo.go
@@ -0,0 +1,26 @@
+// Package allgo contains a variety of types, functions and methods to test godoc.
+package allgo
+
+// T is an example exported type.
+type T struct{}
+
+// DeprecatedT is an example of a deprecated exported type.
+//
+// Deprecated: Use T instead.
+type DeprecatedT struct{}
+
+// F is an example exported method on T.
+func (t *T) F() {}
+
+// DeprecatedF is an example exported deprecated method on T.
+//
+// Deprecated: Use F instead.
+func (t *T) DeprecatedF() {}
+
+// F is an exported function.
+func F() {}
+
+// DeprecatedF is a deprecated function.
+//
+// Deprecated: Use F instead.
+func DeprecatedF() {}
